325048e8bb2c04acf2bb10936d569aff0816354c
JamesH <jwhusc7@gmail.com>
2022-03-21 13:10:14 -0400

    added todo functionality to western province page

--- assets/js/checkbox-wp.js
+++ assets/js/checkbox-wp.js
@@ -1,15 +1,176 @@
+// KIBUYE
 var kivuToursCB = document.getElementById('kivu-tours-CB');
-var boxes = {}
-function saveCheck(){
-    if(kivuToursCB.checked){
-        boxes.KivuTours = true
-        localStorage.setItem("boxes", JSON.stringify(boxes));
-        console.log(boxes);
-    }else{
-        boxes.KivuTours = false
-        localStorage.setItem("boxes", JSON.stringify(boxes));
-        console.log(boxes);
+var ilizaCB = document.getElementById('iliza-CB');
+var moeCB = document.getElementById('moe-CB');
+var sailorsRestoCB = document.getElementById('sailors-resto-CB');
+var cormaranCB = document.getElementById('cormaran-CB');
+// KIGUFI
+var amaniToursCB = document.getElementById('amani-tours-CB');
+var elClassicoCB = document.getElementById('el-classico-CB');
+var palmBeachCB = document.getElementById('palm-beach-CB');
+// COUNTRYSIDE
+var karisimbiCB = document.getElementById('karisimbi-CB');
+var nyungweCB = document.getElementById('nyungwe-CB');
+
+
+
+var boxes = JSON.parse(localStorage.getItem('boxes')) || {};
+console.log(boxes);
+for(const property in boxes){
+    switch(property){
+        case "KivuTours":
+            if(boxes[property]){
+                kivuToursCB.checked = true
+            }
+            break;
+        case "ilizaHouseboat":
+            if(boxes[property]){
+                ilizaCB.checked = true
+            }
+            break;
+        case "moe":
+            if(boxes[property]){
+                moeCB.checked = true;
+            }
+            break;
+        case "sailorsResto":
+            if(boxes[property]){
+                sailorsRestoCB.checked = true;
+            }
+            break;
+        case "cormaranLodge":
+            if(boxes[property]){
+                cormaranCB.checked = true;
+            }
+            break;
+        case "amaniTours":
+            if(boxes[property]){
+                amaniToursCB.checked = true;
+            }
+            break;
+        case "elClassico":
+            if(boxes[property]){
+                elClassicoCB.checked = true;
+            }
+            break;
+        case "palmBeach":
+            if(boxes[property]){
+                palmBeachCB.checked = true;
+            }
+            break;
+        case "karisimbi":
+            if(boxes[property]){
+                karisimbiCB.checked = true;
+            }
+            break;
+        case "nyungwe":
+            if(boxes[property]){
+                nyungweCB.checked = true;
+            }
+            break;
+        default:
+            console.log('nothing checked');
+            break;
     }
 }
 
-kivuToursCB.addEventListener("click", saveCheck)
+function saveCheck(event){
+    console.log("CLICKED");
+    var toDo = event.target;
+    if(toDo === kivuToursCB){
+        if (kivuToursCB.checked){
+            boxes.KivuTours = true
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        } else {
+            boxes.KivuTours = false
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        }
+    } else if(toDo === ilizaCB){
+        if (ilizaCB.checked){
+            boxes.ilizaHouseboat = true
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        } else {
+            boxes.ilizaHouseboat = false
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        }
+    } else if(toDo === moeCB){
+        if(moeCB.checked){
+            boxes.moe = true
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        } else {
+            boxes.moe = false
+            localStorage.setItem("boxes", JSON.stringify(boxes));
+        }
+    } else if (toDo === sailorsRestoCB){
+        if(sailorsRestoCB.checked){
+            boxes.sailorsResto = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.sailorsResto = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if (toDo === cormaranCB){
+        if(cormaranCB.checked){
+            boxes.cormaranLodge = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.cormaranLodge = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if (toDo === amaniToursCB){
+        if(amaniToursCB.checked){
+            boxes.amaniTours = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.amaniTours = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if(toDo === elClassicoCB){
+        if(elClassicoCB.checked){
+            boxes.elClassico = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.elClassico = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if (toDo === palmBeachCB){
+        if(palmBeachCB.checked){
+            boxes.palmBeach = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.palmBeach = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if (toDo === karisimbiCB){
+        if(karisimbiCB.checked){
+            boxes.karisimbi = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.karisimbi = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    } else if (toDo === nyungweCB){
+        if(nyungweCB.checked){
+            boxes.nyungwe = true;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        } else {
+            boxes.nyungwe = false;
+            localStorage.setItem('boxes', JSON.stringify(boxes));
+        }
+    }
+    console.log(boxes);
+};
+
+// KIBUYE
+kivuToursCB.addEventListener('click', saveCheck);
+ilizaCB.addEventListener('click', saveCheck);
+moeCB.addEventListener('click', saveCheck);
+sailorsRestoCB.addEventListener('click', saveCheck);
+cormaranCB.addEventListener('click', saveCheck);
+// KIGUFI
+amaniToursCB.addEventListener('click', saveCheck);
+elClassicoCB.addEventListener('click', saveCheck);
+palmBeachCB.addEventListener('click', saveCheck);
+//COUNTRYSIDE
+karisimbiCB.addEventListener('click', saveCheck);
+nyungweCB.addEventListener('click', saveCheck);
--- assets/js/tour-list.js
+++ assets/js/tour-list.js
@@ -7,13 +7,70 @@ function loadList() {
         while(tourList.firstChild){
             tourList.firstChild.remove();
         };
-        var listObj = JSON.parse(localStorage.getItem("boxes"));
-        console.log(listObj);
-        for(const property in listObj){
-            console.log(property)
+        var boxes = JSON.parse(localStorage.getItem("boxes"));
+        console.log(boxes);
+        var introP = document.createElement("p");
+        introP.className = "flow-text center";
+        introP.textContent = "Your adventure: ";
+        tourList.appendChild(introP);
+        for(const property in boxes){
             var locationP = document.createElement("p");
-            locationP.textContent = property
             locationP.className = "flow-text"
+
+            switch(property){
+                case "KivuTours": 
+                    if(boxes[property]){
+                        console.log(boxes[property])
+                        locationP.textContent = "Kivu Tours: Take a boating adventure on Lake Kivu"
+                    }
+                    break;
+                case "ilizaHouseboat":
+                    if(boxes[property]){
+                        locationP.textContent = "Iliza Houseboat: Cruise in comfort and luxury around Lake Kivu"
+                    }
+                    break;
+                case "moe":
+                    if(boxes[property]){
+                        locationP.textContent = "Visit the Musuem of Environment in Kibuye"
+                    }
+                    break;
+                case "sailorsResto":
+                    if(boxes[property]){
+                        locationP.textContent = "Sailors Restaurant and Cafe: Enjoy an upscale meal overlooking Lake Kivu"
+                    }
+                    break;
+                case "cormaranLodge":
+                    if(boxes[property]){
+                        locationP.textContent = "Cormaran Lodge: Lodge in scerenity on the lushious banks of Lake Kivu"
+                    }
+                    break;
+                case "amaniTours":
+                    if(boxes[property]){
+                        locationP.textContent = "Amani Jordan Island Tours: Explore the beautiful islands of Lake Kivu"
+                    }
+                    break;
+                case "elClassico":
+                    if(boxes[property]){
+                        locationP.textContent = "El Classico Beach Chez West: Enjoy delicious Rwandan food on the beach"
+                    }
+                    break;
+                case "palmBeach":
+                    if(boxes[property]){
+                        locationP.textContent = "Palm Beach Resort: Lodge on the beautiful shores of Lake Kivu"
+                    }
+                    break;
+                case "karisimbi":
+                    if(boxes[property]){
+                        locationP.textContent = "Mount Karisimbi: Hike Mount Karisimbi and visit the friendly gorillas"
+                    }
+                    break;
+                case "nyungwe":
+                    if(boxes[property]){
+                        locationP.textContent = "Nyungwe Forest National Park: Hike the trails of the lush Nyungwe Forest and visit the friendly chimpanzees"
+                    }
+                    break;
+            };
+
             tourList.appendChild(locationP);
         };
     } else{
